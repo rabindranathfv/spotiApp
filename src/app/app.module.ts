e5f5b392e14a6dcc3ddbaf3e083c1089efa0cfcb
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-11-28 21:26:34 -0300

    using Router and activatedRoute in navbar for home and search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -24,7 +25,8 @@ import { NavbarComponent } from './components/share/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
